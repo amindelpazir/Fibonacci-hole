cffb21efbebb7f9e58361ee132f887d07ee1589f
amin <65921968+amindelpazir@users.noreply.github.com>
2020-06-06 13:14:06 +0430

    Update start_mining_yenten.bat

--- start_mining_yenten.bat
+++ start_mining_yenten.bat
@@ -1,6 +1,4 @@
-cpuminer.exe -a yespowerr16 -o stratum+tcp://cpu-pool.com:63368 -u WALLET_ADDRESS
-
-rem alternate pool
-rem cpuminer.exe -a yespowerr16 -o stratum+tcp://yenten-pool.info:6233 -u WALLET_NUMBER -p c=YTN
-
-pause
+cd src
+./yentend
+cd ~/.yenten/
+nano yenten.conf
